97f8f06f7e87fd793fd30ddea05306db6f932a67
Emarilyne Wakio <emarilynew@gmail.com>
2024-04-04 14:26:33 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+
+    };
+    
+    return fetch("http://localhost:3000/users", requestOptions)
+       .then(response => {
+        if (!response.ok) {
+            throw new Error('Network issue!');
+        }
+        return response.json();
+       })
+       .then(data => {
+        const id = data.id;
+        document.body.append(id);
+       })
+       .catch(error => {
+        document.body.append(error.message);
+       });
+    
+}
